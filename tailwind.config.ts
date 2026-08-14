dcee0909712323c1db0dd9d0ff867a72fd9d3af6
victorpero <pero.victor@gmail.com>
2026-08-14 08:55:27 +0200

    Restore destructive button styles

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,6 +4,7 @@ const config: Config = {
   content: [
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/features/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/lib/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
